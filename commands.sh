c58c43d691fbbc13485095fe70dbb2679167e098
dangminhtruong <xdangminhtruongx@gmail.com>
2018-06-04 12:52:32 +0700

    init

---
+++ commands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir /root/.ssh
+echo -e $publicKey > /root/.ssh/id_rsa.pub
+echo -e $privateKey > /root/.ssh/id_rsa
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/id_rsa
+chmod 644 /root/.ssh/id_rsa.pub
+chmod 644 /root/.ssh/authorized_keys
+cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tIdentityFile /root/.ssh/id_rsa\n\tAddKeysToAgent yes\n" > /root/.ssh/config
+ssh-keyscan github.com > /root/.ssh/known_hosts
+chmod -R 644 /root/.ssh/known_hosts
+mkdir /root/project
+chmod -R 777 /root/project
+cd /root/project && git clone $cloneUrl
+if ! [[ $target =~ '^[0-9]+$' ]]; then
+   git checkout $target
+else
+   echo $target
+fi
