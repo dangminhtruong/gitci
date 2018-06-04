d1827d669ac4f07a4c90f7e87017d404ec0df261
dangminhtruong <xdangminhtruongx@gmail.com>
2018-06-04 13:44:17 +0700

    push done

--- commands.sh
+++ commands.sh
@@ -11,9 +11,7 @@ echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tIdentityFile /root/.ssh/
 ssh-keyscan github.com > /root/.ssh/known_hosts
 chmod -R 644 /root/.ssh/known_hosts
 mkdir /root/project
-chmod -R 777 /root/project
 cd /root/project && git clone $cloneUrl
-chmod -R 777 *
 if [[ $target =~ ^-?[0-9]+$ ]]; then
    echo Pullrequest
 else
